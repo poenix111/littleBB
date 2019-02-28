93f9b9385142e6fbd69a5ab45ded68ba1bcfb915
poenix111 <nazazuke@gmail.com>
2019-02-28 10:40:18 -0600

    Agregue dos componentes alerta y datagrid

---
+++ src/app/components/alerta/alerta.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-alerta',
+  templateUrl: './alerta.component.html',
+  styleUrls: ['./alerta.component.scss'],
+})
+export class AlertaComponent implements OnInit {
+
+  constructor(private alertController: AlertController) { }
+
+  ngOnInit() {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+}
---
+++ src/app/components/datagrid/datagrid.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datagrid',
+  templateUrl: './datagrid.component.html',
+  styleUrls: ['./datagrid.component.scss'],
+})
+export class DatagridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
