0dafc830900aab9961ec2136379ea111d310e565
poenix111 <nazazuke@hotmail.com>
2019-03-11 14:35:03 -0600

    Pagina para mostrar los libros en cards

--- src/app/mostrar-libros/mostrar-libros.page.ts
+++ src/app/mostrar-libros/mostrar-libros.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import {Global} from '../global';
 @Component({
   selector: 'app-mostrar-libros',
   templateUrl: './mostrar-libros.page.html',
   styleUrls: ['./mostrar-libros.page.scss'],
 })
 export class MostrarLibrosPage implements OnInit {
-
-  constructor() { }
+  buscar = '';
+  libros = [];
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
+
   }
 
+  mostrar(){
+
+    if(this.buscar === '*'){
+      const useful = Global.dominio + '/recuperar-libros/';
+      this.http.get(useful).subscribe(data =>{
+// tslint:disable-next-line: forin
+        for(let l in data){
+          this.libros.push(data[l]);
+        }
+        console.log(this.libros);
+      }, error =>{
+
+      });
+
+
+    } else{
+      console.log('building');
+    }
+
+  }
 }
